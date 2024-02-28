6d7cbafb1bd6826fe328faa7e6b916c1f231ddf0
feathere <wutsdadiffrence@gmail.com>
2024-02-28 06:30:34 -0800

    Updated script to read from different files

---
+++ programs/scripts/one_liners.py
@@ -0,0 +1,32 @@
+import random
+import sys
+
+# Change the location for your list of pickup lines
+CAT_FACTS_FILE_PATH = 'C:/Users/eunic/IdeaProjects/twitch-commands/programs/resources/cat_facts.txt'
+DAD_JOKES_FILE_PATH = 'C:/Users/eunic/IdeaProjects/twitch-commands/programs/resources/dad_jokes.txt'
+DOG_FACTS_FILE_PATH = 'C:/Users/eunic/IdeaProjects/twitch-commands/programs/resources/dog_facts.txt'
+DIARY_FILE_PATH = 'C:/Users/eunic/IdeaProjects/twitch-commands/programs/resources/diary.txt'
+PICKUP_LINES_FILE_PATH = 'C:/Users/eunic/IdeaProjects/twitch-commands/programs/resources/pickup_lines.txt'
+
+# Switch statement
+match sys.argv[1]:
+    case "Cat Facts":
+        file_to_read = CAT_FACTS_FILE_PATH
+    case "Dad Jokes":
+        file_to_read = DAD_JOKES_FILE_PATH
+    case "Diary":
+        file_to_read = DIARY_FILE_PATH
+    case "Dog Facts":
+        file_to_read = DOG_FACTS_FILE_PATH
+    case "Pickup Lines":
+        file_to_read = PICKUP_LINES_FILE_PATH
+    case _:
+        file_to_read = ''
+
+# Open the file in read mode
+with open(file_to_read, "r", encoding="utf8") as file:
+    all_text = file.read()
+    line = list(map(str, all_text.split("\n")))
+
+    # Print random line from text file
+    print(random.choice(line))
